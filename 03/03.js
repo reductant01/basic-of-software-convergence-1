e95e326175f918690e8dc56b3cfc53d879f6eab0
reductant01 <reductant01@pusan.ac.kr>
2024-07-01 11:00:47 +0900

    단위환산

--- 03/03.js
+++ 03/03.js
@@ -1,20 +1,21 @@
-// document.addEventListener("DOMContentLoaded", function() {
-//     console.log("안녕!!")
-// });
 
-document.addEventListener("DOMContentLoaded", ()=>{
+// document.addEventListener("DOMContentLoaded", function () { 
+//   console.log('안녕!!')
+// }) ;
+
+document.addEventListener("DOMContentLoaded", () => {
     console.log("화살표 함수!!")
 
     //변수선언
-    // const msg = document.getElementById("msg") 
-    const msg = document.querySelector("#msg")
-    const bt = document.querySelector("button") 
+    // const msg = document.getElementById('msg') ;
+    const msg = document.querySelector('#msg');
+    const bt = document.querySelector('button');
 
-    //msg.innerHTML = "<h1>안녕</h1>" (h1을 변역하여 글자 크기 수정)
-    //msg.textContent = "<h1>안녕</h1>" (내용 모두 출력)
+    // msg.innerHTML = '<h1>안녕</h1>' ;
+    // msg.textContent = '<h1>안녕</h1>' ;
 
-    bt.addEventListener("click", ()=>{
-        if (msg.innerHTML == "안녕") msg.innerHTML = "잘가~~"
-        else msg.innerHTML == "안녕"
-    })
+    bt.addEventListener("click", () => {
+        if (msg.innerHTML == '안녕') msg.innerHTML = "잘가~~";
+        else msg.innerHTML = "안녕";
+    });
 });
